7264c3e489ce8d04e5f57707e4fc7b830050349c
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-06-29 11:24:02 +0000

    [add]creatアクション

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get 'books/index'
   # get 'books/edit'
   # get 'books/show'
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
