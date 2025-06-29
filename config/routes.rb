c62f03ef205ef9f8c98f44407cd645703093b527
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-06-29 10:35:27 +0000

    [edit]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/edit'
-  get 'books/show'
-  get 'homes/top'
+  # get 'homes/top'　下に変更
+  # /topでhomes#topを実行する
+  get '/top' => 'homes#top'
+
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/edit'
+  # get 'books/show'
+  resources :books, only: [:new, :index, :show, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
